42a0abee4bc19e6fcda714a2698475334ac9e028
web20082001 <web20082001@gmail.com>
2015-02-16 22:09:11 +0800

    格式化
    格式化config.php

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,9 +1,6 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-    'DEFAULT_MODULE'     => 'Index', //默认模块
-    'URL_MODEL'          => '2', //URL模式
-    'SESSION_AUTO_START' => true, //是否开启session
-    //更多配置参数
-
+    'DEFAULT_MODULE'     => 'Index',		//默认模块
+    'URL_MODEL'          => '2', 			//URL模式
+    'SESSION_AUTO_START' => true 			//是否开启session
 );
